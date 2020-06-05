eda830a2f054168cc225d7d66c6ee6bd71194941
Prabhat Kadam <prabhatkadam@Prabhats-MacBook-Air.local>
2020-06-05 14:43:30 +0530

    fixes

--- app.js
+++ app.js
@@ -138,8 +138,8 @@ app.post("/register", (req, res) => {
       }
     } else {
       passport.authenticate("local")(req, res, function(){
-        createUserFolder(req.user._id);
-        pwd = `/uploads/${req.user._id}/`;
+        createUserFolder(req.user.id);
+        pwd = `/uploads/${req.user.id}/`;
         res.sendStatus(200);
       });
     }
